f23c76c7b83f02ea464757ef4853c70fda894c01
doubleZ <doubleZ0108@163.com>
2021-01-09 21:32:30 +0800

    refactor for code & add comment

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@
 '''
 @program: generate.py
 
-@description: 
+@description: main function for generating photo wall gallery
 
 @author: doubleZ
 
@@ -18,28 +18,40 @@ import util
 
 
 def generate_photo_wall_gallery(imgUrls, targetSize=(400,400), blur_level=6, resultUrl='img/result/result.png'):
-    w, h = util.crack(len(imgUrls))
+    '''
+    main function for generate photo wall gallery
+    :param imgUrls: a list of images url
+    :param targetSize: size for each small pictures in the joint(must be the same)
+    :param blur_level: blur level for result image(0~10)
+    :param resultUrl: url for result image
+    '''
+    w, h = util.crack(len(imgUrls))     # w and h are the two closest number that w*h=len(imgUrls)
 
+    # read images and resize them
     imgs = []
     for imgUrl in imgUrls:
         img = Image.open(imgUrl).resize(targetSize)
         imgs.append(img)
 
-    joint = Image.new('RGB', (targetSize[0]*h, targetSize[1]*w))
+    # joint result image
+    joint_img = Image.new('RGB', (targetSize[0]*h, targetSize[1]*w))
     for i in range(w):
         for j in range(h):
             index = i * h + j
             loc = (targetSize[0] * j, targetSize[1] * i)
-            joint.paste(imgs[index], loc)
+            joint_img.paste(imgs[index], loc)
 
-    blur_joint = joint if blur_level ==0 else util.blur(np.array(joint), util.map_level(blur_level))
+    # blur the result image
+    blur_joint_img = joint_img if blur_level ==0 else util.blur(np.array(joint_img), util.map_level(blur_level))
 
-    blur_joint.save(resultUrl)
+    blur_joint_img.save(resultUrl)
     print("Photo Wall Gallery done!!!🎉")
 
 
 
 if __name__ == '__main__':
+
+    path = 'img/'       # url for original images
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--size", help="Target size using for resize and uniform picture(400*400 as default) [e.g. (300,400)]",
@@ -49,7 +61,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.size and args.blur:
-        path = 'img/'  # 图片文件夹路径
         imgUrls = util.get_all_images(path)
         generate_photo_wall_gallery(imgUrls, targetSize=tuple(args.size), blur_level=int(args.blur))
     else:
--- util.py
+++ util.py
@@ -3,7 +3,7 @@
 '''
 @program: util.py
 
-@description: 
+@description: useful utils for auxiliary generating
 
 @author: doubleZ
 
@@ -14,40 +14,58 @@ import cv2
 from PIL import Image
 import numpy as np
 
+
 def crack(integer):
+    '''
+    divide an integer into two closest number ($1 * $2)
+    :param integer: integer waiting for divide
+    :return: two factor of this integer, $1 <= $2
+    '''
     start = int(integer ** 0.5)
     factor = integer / start
-    while int(factor) != factor:
+    while int(factor) != factor:    # if it's an integer
         start += 1
         factor = integer / start
-    return start, int(factor)       # start <= factor
+    return start, int(factor)  # start <= factor
 
 
 def get_all_images(path):
+    '''
+    get all images in this path ending with .jpg or .png
+    :param path: path of the image folder
+    :return: a list of images url
+    '''
     result = []
     filelist = os.listdir(path)
     for file in filelist:
         if os.path.isfile(os.path.join(path, file)):
-            if file[-3:] in ('jpg', 'png'):
+            if file[-4:] in ('.jpg', '.png'):
                 result.append(os.path.join(path, file))
     result.sort()
     return result
 
+
 def map_level(level, factor=15):
+    '''
+    map the origin level for magnifying effect
+    :param level: bluring level
+    :param factor: factor for magnifying effect
+    :return: refactor level
+    '''
     level *= factor
-    return level if level%2!=0 else level + 1
+    return level if level % 2 != 0 else level + 1
 
-def blur(img, kernel):
+
+def blur(img, kernel=91):
     '''
-
-    :param img:
-    :param kernel: must be odd number
-    :return:
+    use Gaussian Function to blur an image
+    :param img: origin image in np.array format
+    :param kernel: size of the Gaussian kernel(must be odd number)
+    :return: blured image in PIL.Image format
     '''
-
-    img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
+    img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
     kernel_size = (kernel, kernel)
-    blur_img = cv2.GaussianBlur(img,kernel_size,0)
+    blur_img = cv2.GaussianBlur(img, kernel_size, 0)
 
-    return Image.fromarray(cv2.cvtColor(blur_img,cv2.COLOR_BGR2RGB))
+    return Image.fromarray(cv2.cvtColor(blur_img, cv2.COLOR_BGR2RGB))
